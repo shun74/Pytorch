b3224c27aa877e453f8814ea4a368bca6f1b7514
shun74 <syunnsyunn74@gmail.com>
2021-05-12 19:35:43 +0900

    modules to Conv2dBatchnorm

--- SemanticSegmentation/NetworkModule.py
+++ SemanticSegmentation/NetworkModule.py
@@ -79,4 +79,65 @@ class PSPNet(nn.Module):
         return (output, output_aux)
 
 class conv2DBatchNormRelu(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, padding, dilation, bias):
+    def __init__(self, in_channels, out_channels, stride, kernel_size, padding, dilation, bias):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels,kernel_size,stride,padding, dilation, bias=bias)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.batchnorm(x)
+        outputs = self.relu(x)
+
+        return outputs
+
+class FeatureMap_convolution(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        in_channels, out_channels, kernel_size, stride, padding, dilation, bias = 3, 64, 3 , 2, 1, 1, False
+        self.cbnr_1 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size, stride, padding, dilation, bias)
+
+        in_channels, out_channels, kernel_size, stride, padding, dilation, bias = 64, 64, 3 , 1, 1, 1, False
+        self.cbnr_2 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size, stride, padding, dilation, bias)
+
+        in_channels, out_channels, kernel_size, stride, padding, dilation, bias = 64, 128, 3 , 1, 1, 1, False
+        self.cbnr_3 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size, stride, padding, dilation, bias)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        x = self.cbnr_1(x)
+        x = self.cbnr_2(x)
+        x = self.cbnr_3(x)
+        outputs = self.maxpool(x)
+        return outputs
+
+class ResidualBlockPSP(nn.Sequential):
+    def __init__(self, n_blocks, in_channels, mid_channels, out_channels, stride, dilation):
+        super().__init__()
+
+        self.add_module(
+            "block1",
+            bottleNeckPSP(in_channels, mid_channels, out_channels, stride, dilation)
+        )
+
+        for i in range(2, n_blocks + 1):
+            self.add_module(
+                "block" + str(i),
+                bottleNeckPSP(
+                    out_channels, mid_channels, stride, dilation)
+            )
+
+class conv2DBatchNorm(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, bias):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels,kernel_size, stride, padding, dilation, bias=bias)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        outputs = self.batchnorm(x)
+
+        return outputs
