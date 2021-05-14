656fcb48ae1ac749d6aa165332ce23a9ef2343fc
shun74 <syunnsyunn74@gmail.com>
2021-05-14 16:37:35 +0900

    add bottle neck psp

--- SemanticSegmentation/NetworkModule.py
+++ SemanticSegmentation/NetworkModule.py
@@ -142,3 +142,56 @@ class conv2DBatchNorm(nn.Module):
 
         return outputs
 
+class bottleNeckPSP(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, stride, dilation):
+        super().__init__()
+
+        self.cbr_1 = conv2DBatchNormRelu(
+            in_channels, mid_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+        self.cbr_2 = conv2DBatchNormRelu(
+            mid_channels, mid_channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation, bias=False
+        )
+        self.cb_3 = conv2DBatchNorm(
+            mid_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+
+        self.cb_residual = conv2DBatchNorm(
+            in_channels, out_channels, kernel_size=1, stride=stride, padding=0, dilation=1, bias=False
+            )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        conv = self.cbr_1(x)
+        conv = self.cbr_2(conv)
+        conv = self.cb_3(conv)
+        residual = self.cb_residual(x)
+        return self.relu(conv + residual)
+
+class bottleNeckIdentifyPSP(nn.Module):
+    def __init__(self, in_channels, mid_channels, stride, dilation):
+        super().__init__()
+
+        self.cbr_1 = conv2DBatchNormRelu(
+            in_channels, mid_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+        self.cbr_2 = conv2DBatchNormRelu(
+            mid_channels, mid_channels, kernel_size=3, stride=1, padding=dilation, dilation=dilation, bias=False
+        )
+        self.cb_3 = conv2DBatchNorm(
+            mid_channels, in_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+        )
+
+        self.cb_residual = conv2DBatchNorm(
+            in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False
+            )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        conv = self.cbr_1(x)
+        conv = self.cbr_2(conv)
+        conv = self.cb_3(conv)
+        residual = x
+        return self.relu(conv + residual)
